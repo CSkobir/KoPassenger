c25a9874d32742f887b278b2a5df0218ae0dd367
code-with-nazmul <connectwithnazmul@gmail.com>
2026-05-24 13:24:23 +0600

    Delete src directory

--- src/api/users.js
+++
@@ -1,8 +0,0 @@
-import { api, parseJson } from "./client.js";
-
-export async function getPublicProfile(userId) {
-  const res = await api(`/api/users/${userId}/public`);
-  const body = await parseJson(res);
-  if (!res.ok) throw new Error(body?.error || "Could not load public profile");
-  return body; // { user, reviews }
-}
